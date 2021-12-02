08102f86f62a592a26e947cc77326e7c5cc49c96
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-12-01 22:16:06 -0500

    Primera carga

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Table from "./Table/Table.svelte";
+const types = require("./Table/Column/DefaultTypes.js");
+//export { default as default } from "./Table/Table.svelte";
+export { Table, types as ColumnTypes };
