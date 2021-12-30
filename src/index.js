25616126457cc0e1dd7d853990d5dfb48b7f3fdf
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-12-29 22:32:40 -0500

    Se agrega store para guardar los cambios de las tablas que se notifican por websocket - se lo mueve a pgapi-base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Table from "./Table/Table.svelte";
 const types = require("./Table/Column/DefaultTypes.js");
-import DialogModal  from "./Dialog/Modal.svelte";
+import DialogModal from "./Dialog/Modal.svelte";
+//import { storeChangedTables, storeChangedTablesUpdate }  from "./Table/storeChangedTables.js";
+
 export { Table, types as ColumnTypes, DialogModal };
 //export { default as DialogModal } from "./Dialog/Modal.svelte";
