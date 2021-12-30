d35f92df6e0e376781f243799253a596ca308b93
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-12-30 17:11:42 -0500

    Se agrega storeChangedTables.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Table from "./Table/Table.svelte";
 const types = require("./Table/Column/DefaultTypes.js");
 import DialogModal from "./Dialog/Modal.svelte";
+import {storeChangedTables, socketIoClient} from "./socketio-client";
 //import { storeChangedTables, storeChangedTablesUpdate }  from "./Table/storeChangedTables.js";
 
-export { Table, types as ColumnTypes, DialogModal };
+export { Table, types as ColumnTypes, DialogModal, storeChangedTables, socketIoClient };
 //export { default as DialogModal } from "./Dialog/Modal.svelte";
