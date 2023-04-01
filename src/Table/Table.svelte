c8beeefa4ef581369333332ffddb1eb4bda781c1
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz@farmaenlace.com>
2023-04-01 07:12:20 -0500

    Se quita color al header

--- src/Table/Table.svelte
+++ src/Table/Table.svelte
@@ -722,7 +722,7 @@
       class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
     >
       <!-- Aqui escribe el encabezado de la tabla -->
-      <thead class="has-background-grey-lighter!important">
+      <thead>
         <tr>
           <th class="has-text-centered">#</th>
           {#if SelectionType == 1}
