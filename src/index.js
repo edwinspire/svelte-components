a2c0246fc507860589817bf4c6663b7999e19935
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz@farmaenlace.com>
2023-06-01 00:08:32 -0500

    Export  Input Predictive.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Table from "./Table/Table.svelte";
 import * as types from "./Table/Column/DefaultTypes.js";
 import DialogModal from "./Dialog/Modal.svelte";
+import Predictive from "./Input/Predictive.svelte";
 import {storeChangedTables, WebSocketClient} from "./class/websocket.js";
 //import { storeChangedTables, storeChangedTablesUpdate }  from "./Table/storeChangedTables.js";
 
-export { Table, types as ColumnTypes, DialogModal, storeChangedTables, WebSocketClient };
+export { Table, types as ColumnTypes, DialogModal, storeChangedTables, WebSocketClient, Predictive as PredictiveInput};
 //export { default as DialogModal } from "./Dialog/Modal.svelte";
