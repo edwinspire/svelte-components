8b9ba3749c6f3924edce12b3e891c235ef3350dd
Edwin De La Cruz <edwinspire@gmail.com>
2023-11-24 02:37:31 -0500

    Corrección de bug en la carga de datos a DataTable ya que tenía un error cuando había registros undefined

---
+++ src/lib/Table/Column/BooleanIcon.svelte
@@ -0,0 +1,33 @@
+<script>
+	'use strict';
+	export let value;
+	export const row = {};
+	export let props = {};
+  
+  /*
+  {
+		ontrue: { label: 'true', iconClass: 'fa-regular fa-square-check', showLabel: false },
+		onfalse: { label: 'false', iconClass: 'fa-regular fa-square', showLabel: true }
+	};
+*/
+
+	//  console.log(option, value);
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<td class="has-text-centered" on:click>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<span
+		class="icon-text"
+		on:click={() => {
+			value = !value;
+		}}
+	>
+		<span class="icon">
+			<i class='fa-regular fa-square-check' />
+		</span>
+		
+    <span>{value}</span>
+		
+	</span>
+</td>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script>
-	import { Table, DialogModal, Plotter, Menu} from '../lib/index.js';
+	import { Table, DialogModal, Plotter, Menu, ColumnTypes } from '../lib/index.js';
 	import { onMount } from 'svelte';
 
-	let dataTest = [];
+	let dataTest = [{ checsk: false, name: '33112321', datas: 'sdasdsad' }, { checsk: true, name: '33ds112321', datas: 'sdasdsadddd' }];
 
-	let arriba = false;
-	let cantidad = 0;
 
-	onMount(() => {
-		
-		
-	});
+	
+	let columns = { checsk: {label: 'CHECK', }, name: {label: 'Nombre'} };
+
+	onMount(() => {});
 </script>
-<Menu></Menu>
+
+<Table {columns} bind:RawDataTable ={dataTest} />
