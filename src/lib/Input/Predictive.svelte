589436dbe23cc1ab84d7bb9087e0268e9f646bf1
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-01-03 18:15:15 -0500

    Upgrade Input Predictive

--- src/lib/Input/Predictive.svelte
+++ src/lib/Input/Predictive.svelte
@@ -1,8 +1,8 @@
 <!-- App.svelte -->
 <script>
-	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
-
+	//	import { createEventDispatcher } from 'svelte';
+	//	const dispatch = createEventDispatcher();
+	/*
 	export let options = [
 		{ name: 'Manzana', value: '1' },
 		{ name: 'Durazno', value: '2' },
@@ -25,16 +25,31 @@
 		{ name: 'Taxo', value: '19' },
 		{ name: 'Tamarindo', value: '20' }
 	];
+*/
+	let {
+		options = $bindable([
+			{ name: 'Manzana', value: '1' },
+			{ name: 'Durazno', value: '2' }
+		]),
+		label = $bindable(''),
+		selectedValue = $bindable(''),
+		classLabel = $bindable('is-normal'),
+		classInput = $bindable(''),
+		placeholder = $bindable('Type to see options'),
+		onselect = () => {}
+	} = $props();
 
-	let filteredOptions = options;
-	let showDropdown = false;
-	let inputValue = '';
-	export let label = '';
+	let filteredOptions = $state(options);
+	let showDropdown = $state(false);
+	let inputValue = $state('');
 
+	//	export let label = '';
+	/*
 	export let selectedValue = '';
 	export let classLabel = 'is-normal';
 	export let classInput = '';
 	export let placeholder = 'Type to see options';
+*/
 
 	function handleInput(event) {
 		inputValue = event.target.value;
@@ -49,7 +64,7 @@
 		selectedValue = option.value;
 		showDropdown = false;
 		console.log('option', option);
-		dispatch('select', option);
+		onselect(option);
 	}
 
 	function handleFocus() {
@@ -62,7 +77,7 @@
 
 <div class="field is-horizontal">
 	<div class="field-label {classLabel}">
-		<!-- svelte-ignore a11y-label-has-associated-control -->
+		<!-- svelte-ignore a11y_label_has_associated_control -->
 		<label class="label">{label}</label>
 	</div>
 	<div class="field-body">
@@ -73,8 +88,8 @@
 						class="input {classInput}"
 						type="text"
 						bind:value={inputValue}
-						on:input={handleInput}
-						on:focus={handleFocus}
+						oninput={handleInput}
+						onfocus={handleFocus}
 						{placeholder}
 					/>
 					<input type="hidden" bind:value={selectedValue} />
@@ -84,8 +99,8 @@
 						<div class="dropdown-menu" role="menu">
 							<div class="dropdown-content">
 								{#each filteredOptions as option}
-									<!-- svelte-ignore a11y-invalid-attribute -->
-									<a href="#" class="dropdown-item" on:click={() => handleClick(option)}>
+									<!-- svelte-ignore a11y_invalid_attribute -->
+									<a href="#" class="dropdown-item" onclick={() => handleClick(option)}>
 										{option.name}
 									</a>
 								{/each}
