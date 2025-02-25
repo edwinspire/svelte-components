df5db2e8da72eab9545520b0e3b157025c2db4b5
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-02-24 23:13:35 -0500

    Last build.

--- dist/class/utils.js
+++ dist/class/utils.js
@@ -1,5 +1,6 @@
 export const equalObjs = (value, new_value) => {
 	let new_value_str = typeof new_value == 'object' ? JSON.stringify(new_value) : new_value;
 	let currect_value_str = typeof value == 'object' ? JSON.stringify(value) : value;
+	console.log(value, new_value, new_value_str , currect_value_str);
 	return new_value_str == currect_value_str;
 };
