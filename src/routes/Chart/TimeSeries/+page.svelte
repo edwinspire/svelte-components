ed0aeee1df4f2a80ef0a315ea3708d32a6c1bd15
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-10-12 01:10:32 -0500

    Test echarts

---
+++ src/routes/Chart/TimeSeries/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Chart from '$lib/Chart/TimeSeries/index.svelte';
+	let markdownContent = '## Hola';
+</script>
+
+<Chart bind:markdown={markdownContent} />
---
+++ src/routes/Plotter/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { Plotter } from '$lib/index.js';
+	let markdownContent = '## Hola';
+</script>
+
+<Plotter bind:markdown={markdownContent} />
