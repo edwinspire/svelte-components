65ef7d7d7659ddf4a047eea66973d914136e4e6a
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-11-25 09:12:00 -0500

    Last build

--- dist/EditorCode/EditorCode.svelte.d.ts
+++ dist/EditorCode/EditorCode.svelte.d.ts
@@ -1,16 +1,8 @@
 export default EditorCode;
-type EditorCode = {
-    $on?(type: string, callback: (e: any) => void): () => void;
-    $set?(props: Partial<$$ComponentProps>): void;
-} & {
-    setCode: (new_code: any) => void;
-    getCode: () => any;
-    reset: () => void;
-};
-declare const EditorCode: import("svelte").Component<{
+type EditorCode = SvelteComponent<{
+    left?: any;
+    right?: any;
     code?: string;
-    left: any;
-    right: any;
     lang?: string;
     showFormat?: boolean;
     showSelectLang?: boolean;
@@ -18,16 +10,25 @@ declare const EditorCode: import("svelte").Component<{
     showHiddenButton?: boolean;
     showResetButton?: boolean;
     showCode?: boolean;
-    onchange?: Function;
+    onchange?: any;
+    setCode?: (newCode: any) => void;
+    getCode?: () => any;
+    reset?: () => void;
 }, {
-    setCode: (new_code: any) => void;
+    change: CustomEvent<any>;
+} & {
+    [evt: string]: CustomEvent<any>;
+}, {}> & {
+    $$bindings?: string;
+} & {
+    setCode: (newCode: any) => void;
     getCode: () => any;
     reset: () => void;
-}, "code" | "lang" | "showFormat" | "showSelectLang" | "isReadOnly" | "showHiddenButton" | "showResetButton" | "showCode">;
-type $$ComponentProps = {
+};
+declare const EditorCode: $$__sveltets_2_IsomorphicComponent<{
+    left?: any;
+    right?: any;
     code?: string;
-    left: any;
-    right: any;
     lang?: string;
     showFormat?: boolean;
     showSelectLang?: boolean;
@@ -35,5 +36,29 @@ type $$ComponentProps = {
     showHiddenButton?: boolean;
     showResetButton?: boolean;
     showCode?: boolean;
-    onchange?: Function;
-};
+    onchange?: any;
+    setCode?: (newCode: any) => void;
+    getCode?: () => any;
+    reset?: () => void;
+}, {
+    change: CustomEvent<any>;
+} & {
+    [evt: string]: CustomEvent<any>;
+}, {}, {
+    setCode: (newCode: any) => void;
+    getCode: () => any;
+    reset: () => void;
+}, string>;
+interface $$__sveltets_2_IsomorphicComponent<Props extends Record<string, any> = any, Events extends Record<string, any> = any, Slots extends Record<string, any> = any, Exports = {}, Bindings = string> {
+    new (options: import("svelte").ComponentConstructorOptions<Props>): import("svelte").SvelteComponent<Props, Events, Slots> & {
+        $$bindings?: Bindings;
+    } & Exports;
+    (internal: unknown, props: Props & {
+        $$events?: Events;
+        $$slots?: Slots;
+    }): Exports & {
+        $set?: any;
+        $on?: any;
+    };
+    z_$$bindings?: Bindings;
+}
